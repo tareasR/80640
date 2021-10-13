27a747a1bd455a1ae0543403244b40e2730fbd20
tareasR <tareasrojano@gmail.com>
2021-10-13 13:08:03 -0500

    backend, frontend, js

--- proyectoLogeo/src/main/java/mx/uv/App.java
+++ proyectoLogeo/src/main/java/mx/uv/App.java
@@ -1,13 +1,61 @@
 package mx.uv;
 
+import static spark.Spark.*;
+import com.google.gson.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static Usuario usuario = new Usuario("1", "jose@gmail.com", "123456");
+    private static Gson gson = new Gson();
+    private static Map<String, Usuario> usuarios = new HashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        /**
+         * Métodos utilizados para atender las llamadas del frontend
+         */
+        before((req, res) -> res.type("application/json"));
+        get("/", (req, res) -> gson.toJson(usuario));
+
+        get("/usuarios", (req, res) -> gson.toJson(usuarios.values()));
+
+        post("/", (req, res) -> {
+            // Insertamos un nuevo usuario
+            String json = req.body();
+            Usuario u = gson.fromJson(json, Usuario.class);
+            String id = UUID.randomUUID().toString();
+            u.setId(id);
+            usuarios.put(id, u);
+
+            JsonObject respuesta = new JsonObject();
+            respuesta.addProperty("status", "creado");
+            respuesta.addProperty("id", id);
+            return respuesta;
+        });
     }
 }
