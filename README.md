b455ad86ee6bc3d9875aa483171573bc01442022
tareasR <tareasrojano@gmail.com>
2022-02-02 14:52:43 -0600

    reset

--- README.md
+++
@@ -1,5 +0,0 @@
-"# temporal2" 
-un pequeño cambio
-ejemplo de cambio
-
-	un nuevo cambio
--- calculadora.js
+++
@@ -1,8 +0,0 @@
-var btnUno = document.getElementById("1");
-btnUno.addEventListener("click",function () {
-    document.getElementById("pantalla").value += "1"
-})
-var btnDos = document.getElementById("2");
-btnDos.addEventListener("click",function () {
-    document.getElementById("pantalla").value += "2"
-})
