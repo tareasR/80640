cb65292c8b3922c673f0e3ae4115855edb346ed3
ejemplo <clear>
2022-11-03 13:02:42 -0600

    axios

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -11,6 +11,21 @@ import com.google.gson.JsonObject;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        options("/*", (request, response) -> {
+                String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+                if (accessControlRequestHeaders != null) {
+                    response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+                }
+                String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+                if (accessControlRequestMethod != null) {
+                    response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                }
+                return "OK";
+            });
+            before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+            
+
+
         get("/hello", (req, res) -> "Hello World");
         get("/eduardo", (req, res) -> "Hello Eduardo");
         get("/alex", (req, res) -> "Hello Alex");
@@ -19,7 +34,8 @@ public class App {
         get("/", (req, res) -> "hola");
         
         post("/", (req, res) -> {
-            System.out.println(req.queryParams("email") + " " + req.queryParams("password"));
+            System.out.println(req.queryParams("email") + " " + 
+                req.queryParams("password"));
             System.out.println(req.body());
             res.status(200);// Codigo de respuesta
             JsonObject oRespuesta = new JsonObject();
