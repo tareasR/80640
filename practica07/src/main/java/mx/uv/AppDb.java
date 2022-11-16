5e2143125a1f8ca4c6f4580521dfb818b73f99af
ejemplo <clear>
2022-11-16 13:40:50 -0600

    agregado index.html POST

--- practica07/src/main/java/mx/uv/AppDb.java
+++ practica07/src/main/java/mx/uv/AppDb.java
@@ -13,11 +13,26 @@ import com.google.gson.Gson;
  */
 public class AppDb {
     public static Gson gson = new Gson();
+
     // base de datos en memoria
     // public static Map<String, Usuario> usuarios = new HashMap<>();
     public static void main(String[] args) {
-        
+
         port(80);
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+        before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
+
         // inicialización de datos
         // Usuario u1 = new Usuario("1", "pablo", "1234");
         // Usuario u2 = new Usuario("2", "ana", "7890");
@@ -25,16 +40,17 @@ public class AppDb {
         // usuarios.put(u2.getId(), u2);
 
         System.out.println("Hello World!");
-        before((req, res)-> res.type("application/json"));
+        before((req, res) -> res.type("application/json"));
         // get("/usuario", (req, res) -> gson.toJson(u1));
         // get("/usuarios", (req, res) -> gson.toJson(usuarios));
         get("/usuarios", (req, res) -> gson.toJson(DAO.dameUsuarios()));
 
-        post("/", (req, res)->{
+        post("/", (req, res) -> {
             String datosFormulario = req.body();
             Usuario u = gson.fromJson(datosFormulario, Usuario.class);
             // usuarios.put(u.getId(), u);
-            return "usuario agregado";
+            // return "usuario agregado";
+            return DAO.crearUsuario(u);
         });
     }
 }
