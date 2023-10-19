e64bfc177594b0aa690846410676c9825e7ebdd1
tareasr <tareasrojano@gmail.com>
2023-10-19 12:55:01 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,5 +1,7 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hola Mundo!" );
+        get("/",
+            (request, response)-> "<h1>Hola mundo!</h1>"
+        );
+        get("/ruta1",
+            (request, response)-> "<h1>Adios mundo!</h1>"
+        );
+        get("/ruta2",
+            (request, response)-> "<h1>Que tal mundo!</h1>"
+        );
+        get("/ruta3",
+            (request, response)-> "{'alumno':'john','matricula':'s0001','carrera':'tc'}"
+        );
     }
 }
