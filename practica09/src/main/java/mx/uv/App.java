4254a248af71851a3b3a66e7f393188fadaba614
tareasr <tareasrojano@gmail.com>
2023-11-08 13:45:32 -0600

    CORS

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -11,6 +11,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hola Mundo!" );
+
+    //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+    options("/*",(request,response)->{
+    String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+    if(accessControlRequestHeaders!=null){
+    response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+    }
+    String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+    if(accessControlRequestMethod!=null){
+    response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+    }
+    return "OK";
+    });
+    
+    before((request,response)->response.header("Access-Control-Allow-Origin","*"));
+    
+
         get("/",
             (request, response)-> "<h1>Hola mundo!</h1>"
         );
