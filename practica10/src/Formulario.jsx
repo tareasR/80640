bb8a4aa570b9dc36702388569df93a13ff862b90
tareasr <tareasrojano@gmail.com>
2023-11-16 11:54:54 -0600

    practica10 finalizada

--- practica10/src/Formulario.jsx
+++ practica10/src/Formulario.jsx
@@ -11,7 +11,7 @@ function Formulario (props) {
 
     const hacerPeticion = async () => {
         try {
-            const response = await axios.get('http://localhost:4567/ruta3')
+            const response = await axios.post('http://localhost:4567/ruta2',datosFormulario )
             console.log(response.data)
             return response.data
         } catch (error) {
